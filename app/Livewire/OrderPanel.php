9f9eded2fe989c185a5b7317d992c8f9e2547527
Omwitsa <omwitsawilson@yahoo.com>
2024-04-14 17:05:04 +0300

    Get selected radio button change value

--- app/Livewire/OrderPanel.php
+++ app/Livewire/OrderPanel.php
@@ -12,4 +12,15 @@ class OrderPanel extends Component
     {
         return view('livewire.order-panel');
     }
+
+    // public function updatedBrands($value)
+    // {
+    //     // dd($this->selectedValue);
+    //     // Do something when the selected value changes
+    // }
+
+    // public function creatVariety()
+    // {
+    //     // $this->count--;
+    // }
 }
--- resources/views/livewire/order-panel.blade.php
+++ resources/views/livewire/order-panel.blade.php
@@ -9,31 +9,34 @@
                 <div class="col-xs-12 col-sm-12">
                     <h4 class="sub-title">Brands</h4>
                     <div class="form-check form-check-inline">
-                        <input wire:model="brands" class="form-check-input" type="radio" id="roses" value="Roses">
+                        <input wire:model.change="brands" class="form-check-input" type="radio" id="roses" value="Roses">
                         <label class="form-check-label" for="roses">Roses</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input wire:model="brands" class="form-check-input" type="radio" id="bellissima" value="Bellissima">
+                        <input wire:model.change="brands" class="form-check-input" type="radio" id="bellissima" value="Bellissima">
                         <label class="form-check-label" for="bellissima">Bellissima</label>
                     </div>
                 </div>
             </div><br>
             <div class="row">
                 <div class="col-xs-12 col-sm-12">
-                    <h4 class="sub-title">Range</h4>
-                    
+                    <!-- <h4 class="sub-title">Range</h4> -->
+                    @if($brands === 'Roses')
+                        <p>Roses</p>
+                    @elseif($brands === 'Bellissima')
+                        <p>Bellissima</p>
+                    @endif
                 </div>
             </div><br>
             <div class="row">
                 <div class="col-xs-12 col-sm-12">
                     <!-- Dropdown -->
-                    <h4 class="sub-title">Variety</h4>
+                    <!-- <h4 class="sub-title">Variety</h4> -->
                     
                 </div>
             </div><br>
             <div class="row">
                 <div class="col-xs-12 col-sm-12">
-                    {{$brands}}
                 </div>
             </div><br>
             <button type="submit" class="btn btn-primary waves-effect waves-light">Submit</button>
